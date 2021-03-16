9e306282b61d86284a8374fcb0fb799d41c91962
Nakiyah Fears <fkiyah@yahoo.com>
2021-03-16 16:40:21 -0400

    fixing handlebars and css

--- server.js
+++ server.js
@@ -29,11 +29,11 @@ app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 
 //For Handlebars
-app.set('views', './app/views')
-app.engine('hbs', exphbs({
-    extname: '.hbs'
-}));
-app.set('view engine', '.hbs'); 
+// app.set('views', './app/views')
+// app.engine('hbs', exphbs({
+//     extname: '.hbs'
+// }));
+// app.set('view engine', '.hbs'); 
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -97,7 +97,7 @@ app.get('/javascriptroom',(req,res) => res.render('javascript', {layouts: 'main'
 app.get('/', (req, res) => res.render('index', {layouts: 'main'}));
 
 
-app.get('/login', (req, res) => res.render('login', {layouts: 'main'}));
+app.get('/signin', (req, res) => res.render('signin', {layouts: 'main'}));
 
 // require('./routes/api-routes.js')(app);
 
